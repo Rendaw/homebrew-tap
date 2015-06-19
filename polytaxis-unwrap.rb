88f8edc870a3f0ccbd58e0b4e7bd5f4c00dee32c
Rendaw <rendaw@zarbosoft.com>
2015-06-19 21:13:52 +0900

    first commit

---
+++ polytaxis-unwrap.rb
@@ -0,0 +1,9 @@
+class PolytaxisPython < Formula
+  homepage "https://github.com/rendaw/polytaxis-unwrap"
+  url "https://github.com/rendaw/polytaxis-unwrap/archive/1.0.0.tar.gz"
+  sha1 "0a323c749241b0dcb313b3d4cc9411d8b34bbf75"
+
+  def install
+  	bin.install "formalfriday"
+  end
+end
