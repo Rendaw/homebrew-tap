d4f6dee0fc9ed1bc5243375fa571e6f994c0b7a1
Rendaw <rendaw@zarbosoft.com>
2015-06-21 16:35:06 +0900

    Unwrap fixes

--- polytaxis-unwrap.rb
+++ polytaxis-unwrap.rb
@@ -3,12 +3,20 @@ class PolytaxisUnwrap < Formula
   homepage "https://github.com/Rendaw/polytaxis-unwrap/"
   head "https://github.com/Rendaw/polytaxis-unwrap.git", :using => :git
 
-  depends_on :osxfuse
-  depends_on "tup"
+  depends_on "gcc5" if OS.mac?
+  depends_on :osxfuse if OS.mac?
+  depends_on "tup" if OS.mac?
 
   def install
     system "tup", "init"
     system "cp", "tup.template.config", "tup.config"
+    inreplace "path" do |s|
+      s.gsub! /CONFIG_COMPILERBIN/, "g++-5"
+      s.gsub! /CONFIG_CCOMPILERBIN/, "gcc-5"
+      s.gsub! /CONFIG_LINKERBIN/, "g++-5"
+      s.gsub! /CONFIG_BUILDFLAGS/, "-I/usr/local/include/osxfuse/ -I/usr/local/include/osxfuse/fuse/"
+      s.gsub! /CONFIG_LINKFLAGS/, "-L/usr/local/lib -losxfuse -liconv"
+    end if OS.mac?
     system "tup", "upd"
     bin.install "polytaxis-unwrap"
   end
