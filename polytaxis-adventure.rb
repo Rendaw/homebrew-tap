cd2b687fa9e3604172b329702b558c34301f7d6d
Rendaw <rendaw@zarbosoft.com>
2015-06-21 17:17:35 +0900

    adventure fixes.

--- polytaxis-adventure.rb
+++ polytaxis-adventure.rb
@@ -1,14 +1,10 @@
 $pypi_tuples = [
   ["appdirs", "1.4.0", "8fc245efb4387a4e3e0ac8ebcc704582df7d72ff6a42a53f5600bbb18fdaadc5"],
   ["argh", "0.26.1", "06a7442cb9130fb8806fe336000fcf20edf1f2f8ad205e7b62cec118505510db"],
-  ["ExifRead", "2.1.1", "07319ac6adf9cfc76ae34a357f12bc0b6668fa1513f8e925cf1743189e9cc56d"],
   ["natsort", "4.0.1", "48fde822e66a7f08de8ecf29367f4007c625f6d6260c9ce17f41d0f0c6aac687"],
   ["pathtools", "0.1.2", "7c35c5421a39bb82e58018febd90e3b6e5db34c5443aaaf742b3f33d4655f1c0"],
   ["patricia-trie", "10", "65a35219bf211b4e4b34bdd9e858c008e1699e23c43b3fb4542726c996966bae"],
-  ["pytaglib", "1.0.3", "43c6b1053d7c30bc1ffefe19f31de3555e0239aafbcfd8f33d82edf705d7ef8f"],
-  ["python-magic", "0.4.6", "903d3d3c676e2b1244892954e2bbbe27871a633385a9bfe81f1a81a7032df2fe"],
   ["PyYAML", "3.11", "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"],
-  ["watchdog", "0.8.3", "7e65882adb7746039b6f3876ee174952f8eaaa34491ba34333ddf1fe35de4162"],
 ]
 
 class PolytaxisAdventure < Formula
